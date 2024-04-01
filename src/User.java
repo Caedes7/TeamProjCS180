4c48b9b9d733217684156003241055a8724d53eb
jeeaa <jahmmed@purdue.edu>
2024-04-01 11:43:01 -0400

    I forgot the User interface Javadoc earlier, just added it.

--- src/User.java
+++ src/User.java
@@ -1,3 +1,11 @@
+/** Purdue University -- CS18000 -- Spring 2024 -- Team Project 1 -- Direct Messaging
+ * This is a program that will allow direct messaging, simultaneously, between several users.
+ * This interface contains all the setters and getters of the NewUser class.
+ *
+ * @author Jeeaan Ahmmed, Ishaan Krishna Agrawal, Pranav Yerram, Michael Joseph Vetter
+ * @version April 1, 2024
+ *
+ */
 public interface User {
     // Setters
     void setName(String name);
