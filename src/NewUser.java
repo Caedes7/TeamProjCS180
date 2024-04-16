2f42296a079260bed70d6c47203738d7ebbc61c2
Joey Vetter <vetter5@purdue.edu>
2024-04-16 10:29:00 -0400

    new

--- src/NewUser.java
+++ src/NewUser.java
@@ -18,7 +18,8 @@ public class NewUser implements INewUser {
     private String email;
     private ArrayList<NewUser> blocked;
     private ArrayList<NewUser> friends;
-    private Map<String, List<Message>> messages = new HashMap<>();
+    private Map<String, List<Message>> sentMessages;
+    private Map<String, List<Message>> receivedMessages;
 
     public NewUser(String name, String username, int age, String password, String email) {
         this.name = name;
@@ -28,68 +29,40 @@ public class NewUser implements INewUser {
         this.email = email;
         this.blocked = new ArrayList<>();
         this.friends = new ArrayList<>();
+        this.sentMessages = new HashMap<>();
+        this.receivedMessages = new HashMap<>();
     }
 
     public NewUser(String username) {
-        username = null;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public ArrayList<NewUser> getBlocked() {
-        return blocked;
-    }
-    public ArrayList<NewUser> getFriends() {
-        return friends;
-    }
-    public List<Message> getMessagesWithUser(String otherUser) {
-        return messages.getOrDefault(otherUser, new ArrayList<>());
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setUsername(String username) {
         this.username = username;
     }
 
-    public void setAge(int age) {
-        this.age = age;
+    // Getter and Setter methods
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getUsername() { return username; }
+    public void setUsername(String username) { this.username = username; }
+    public int getAge() { return age; }
+    public void setAge(int age) { this.age = age; }
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public ArrayList<NewUser> getBlocked() { return blocked; }
+    public void setBlocked(ArrayList<NewUser> blocked) { this.blocked = blocked; }
+    public ArrayList<NewUser> getFriends() { return friends; }
+    public void setFriends(ArrayList<NewUser> friends) { this.friends = friends; }
+
+    public Map<String, List<Message>> getSentMessages() { return sentMessages; }
+    public Map<String, List<Message>> getReceivedMessages() { return receivedMessages; }
+
+    // Methods to manipulate messages
+    public void addSentMessage(String receiverUsername, Message message) {
+        sentMessages.computeIfAbsent(receiverUsername, k -> new ArrayList<>()).add(message);
     }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-    public void setBlocked(ArrayList<NewUser> blocked) {
-        this.blocked = blocked;
-    }
-    public void setFriends(ArrayList<NewUser> friends) {
-        this.friends = friends;
-    }
-    public void addMessage(Message message) {
-        messages.computeIfAbsent(message.getReceiver(), k -> new ArrayList<>()).add(message);
-        messages.computeIfAbsent(message.getSender(), k -> new ArrayList<>()).add(message);
+    public void addReceivedMessage(String senderUsername, Message message) {
+        receivedMessages.computeIfAbsent(senderUsername, k -> new ArrayList<>()).add(message);
     }
 
     public boolean isValidUsername(String userName) {
@@ -142,24 +115,6 @@ public class NewUser implements INewUser {
                 this.age == otherUser.age &&
                 this.password.equals(otherUser.password) &&
                 this.email.equals(otherUser.email);
-    }
-
-    public Map<String, List<Message>> getMessages() {
-        // Create a new map to hold the processed messages
-        Map<String, List<Message>> last100MessagesMap = new HashMap<>();
-
-        messages.forEach((otherUsername, messageList) -> {
-            // Sort messages by timestamp
-            Collections.sort(messageList, Comparator.comparingLong(Message::getTimestamp));
-
-            // Retrieve the last 500 messages or all messages if there are less than 500
-            List<Message> last500Messages = messageList.size() > 100 ? messageList.subList(messageList.size() - 100, messageList.size()) : new ArrayList<>(messageList);
-
-            // Put the sorted list into the map
-            last100MessagesMap.put(otherUsername, last500Messages);
-        });
-
-        return last100MessagesMap;
     }
 
     public String toString() {
