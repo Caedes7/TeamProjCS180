1bfe105fa8744f21c58757de0ce056ecbaa9240f
jeeaa <jahmmed@purdue.edu>
2024-04-16 11:49:56 -0400

    Fixes to Client, ClientHandler, and NewUser (just added method)

--- src/ClientHandler.java
+++ src/ClientHandler.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
-import java.util.ArrayList;
 
 public class ClientHandler extends Thread implements Runnable  {
     private Socket clientSocket;
@@ -18,18 +17,14 @@ public class ClientHandler extends Thread implements Runnable  {
 
     public void run() {
         try (BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
+             PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true)) {
             String inputLine;
             NewUser newSingleUser = new NewUser(null);
 
             do {
-                System.out.println("Client connected");
-
                 inputLine = in.readLine();
-                System.out.println("Client connected1");
 
                 if (inputLine.startsWith("CREATE_USER")) {
-                    System.out.println("Client connected2");
 
                     inputLine = inputLine.substring(11);
                     String[] userDetails = inputLine.split(",");
@@ -38,22 +33,16 @@ public class ClientHandler extends Thread implements Runnable  {
                     int age = Integer.parseInt(userDetails[2]);
                     String password = userDetails[3];
                     String email = userDetails[4];
-                    
-                    
+
                     boolean success = database.createUser(name, username, age, password, email);
-                    
+
                     if (success) {
-                        System.out.println("Client connected3");
                         newSingleUser = new NewUser(name, username, age, password, email);
                         out.println("User created successfully");
                     } else {
                         out.println("Failed to create user");
-                        System.out.println("Client connected4");
-
                     }
                 } else if (inputLine.startsWith("RE")) {
-                    System.out.println("Client connected5");
-
                     inputLine = inputLine.substring(2);
                     String[] userDetails = inputLine.split(",");
                     String username = userDetails[0];
@@ -61,61 +50,61 @@ public class ClientHandler extends Thread implements Runnable  {
 
                     boolean success = server.loginUser(username, password);
                     if (success) {
-                        System.out.println("Client connected6");
-                        //NewUser newSingleUser = new NewUser(name, username, age, password, email);
                         out.println("User logged in successfully");
                     } else {
-                        System.out.println("Client connected7");
-
-                        out.println("Failed to login user");
+                        out.println("User does not exist");
                     }
-                    out.flush();
                 }
-            
-            String choiceString = in.readLine();
-            int choice = Integer.parseInt(choiceString.substring(0,1));
 
-            while (choice != 0) {
-                
-                switch (choice) {
-                    case 1:
-                        NewUser found;
-                        found = database.searchUsers(choiceString.substring(1));
-                        if (found == null) {
-                            out.println(found.toString());
-                        } else {
-                            out.println("User not found");
-                        }
-                        break;
-                    case 2:
-                        boolean blocked;
-                        blocked = database.blockUser(newSingleUser.getUsername(), choiceString.substring(1));
-                        if (!blocked) {
-                            out.println("Could not block user.");
-                        } else {
-                            out.println("User blocked!");
-                        }
-                        break;
-                    case 3:
-                        boolean friend;
-                        friend = database.addFriend(newSingleUser.getUsername(), choiceString.substring(1));
-                        if (!friend) {
-                            out.println("Could not add user as friend.");
-                        } else {
-                            out.println("User friended!");
-                        }
-                        break;
-                    case 4:
-                        String message = "Messages will be implemented later with the GUI.";
-                        out.println(message);
-                        break;
-                    case 0:
-                        System.out.println("Exiting.");
-                        break;
-                    default:
-                        System.out.println("Invalid choice. Please try again.");
+                String choiceString = "";
+                int choice = -1;
+
+                while (choice != 0) {
+                    choiceString = in.readLine();
+                    System.out.println(choiceString);
+                    choice = Integer.parseInt(choiceString.substring(0,1));
+                    switch (choice) {
+                        case 1:
+                            NewUser found;
+                            found = database.searchUsers(choiceString.substring(1));
+                            if (found != null) {
+                                String output = found.toStringSearch() + "\nFurther implementation requires GUI";
+                                out.println(output);
+                            } else {
+                                out.println("User not found");
+                            }
+                            out.flush();
+                            break;
+                        case 2:
+                            boolean blocked;
+                            blocked = database.blockUser(newSingleUser.getUsername(), choiceString.substring(1));
+                            if (!blocked) {
+                                out.println("Could not block user.");
+                            } else {
+                                out.println("User blocked!");
+                            }
+                            break;
+                        case 3:
+                            boolean friend;
+                            friend = database.addFriend(newSingleUser.getUsername(), choiceString.substring(1));
+                            if (!friend) {
+                                out.println("Could not add user as friend.");
+                            } else {
+                                out.println("User friended!");
+                            }
+                            break;
+                        case 4:
+                            String message = "Messages will be implemented later with the GUI.";
+                            out.println(message);
+                            break;
+                        case 0:
+                            System.out.println("Exiting.");
+                            break;
+                        default:
+                            System.out.println("Invalid choice. Please try again.");
+                    }
                 }
-            }
+                break;
             }  while ((inputLine = in.readLine()) != null);
         } catch (Exception e) {
             e.printStackTrace();
