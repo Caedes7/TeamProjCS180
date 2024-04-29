6d914aa6a65ea0326012580633a9b7114e8fc4e8
jeeaa <jahmmed@purdue.edu>
2024-04-29 16:17:45 -0400

    TestCase name fix

--- src/RunLocalTestCase.java
+++ src/RunLocalTestCase.java
@@ -11,7 +11,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Date;
 
-class ClientHandlerTest {
+public class RunLocalTestCase {
 
     private static StringWriter stringWriter;
     private static PrintWriter printWriter;
